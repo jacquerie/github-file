9c63eeefd838f63e4111d0b3d0aa6f97c3864404
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-07-16 00:30:44 +0200

    cli: add working update command
    Sem-ver: adds new feature

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -2,10 +2,28 @@
 
 from __future__ import absolute_import, division, print_function
 
+import pytest
+
 from github_file.cli import cli
 
+GITHUB_FILE = '''\
+[core]
+owner = jacquerie
+repo = github-file
+description = Configure your GitHub repository from a file,
+    without having to click around in the UI.
 
-def test_update_uses_the_provided_filename(runner):
-    result = runner.invoke(cli, ['update', '-f', 'Githubfile'])
+[features]
+has_issues = true
+has_wiki = false
+'''
+
+
+@pytest.mark.vcr()
+def test_update_uses_the_provided_filename(runner, tmpdir):
+    config_fd = tmpdir.join('github-compose.yaml')
+    config_fd.write(GITHUB_FILE)
+
+    result = runner.invoke(cli, ['update', '-f', str(config_fd)])
 
     assert 0 == result.exit_code
