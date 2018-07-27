e8deae3909ac5bcbe264f82eea4908f0c478230d
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2018-07-27 21:43:09 +0200

    tests: write one test per feature

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -2,34 +2,95 @@
 
 from __future__ import absolute_import, division, print_function
 
+from textwrap import dedent
+
 import pytest
 
 from github_file.cli import cli
 
-GITHUB_FILE = '''\
-[core]
-owner = jacquerie
-repo = github-file
-description = Configure your GitHub repository from a file,
-    without having to click around in the UI.
-topics = github, configuration, file
 
-[features]
-has_issues = true
-has_projects = false
-has_wiki = false
+@pytest.mark.vcr()
+def test_update_can_set_the_description(runner, tmpdir):
+    config_fd = tmpdir.join('Githubfile')
+    config_fd.write(dedent(u'''\
+    [core]
+    owner = jacquerie
+    repo = github-file
+    description = Configure your GitHub repository from a file,
+        without having to click around in the UI.
+    '''))
 
-[merges]
-allow_squash_merge = false
-allow_merge_commit = false
-allow_rebase_merge = true
-'''
+    result = runner.invoke(cli, ['update', '-f', str(config_fd)])
+
+    assert 0 == result.exit_code
 
 
 @pytest.mark.vcr()
-def test_update_uses_the_provided_filename(runner, tmpdir):
+def test_update_can_set_the_topics(runner, tmpdir):
     config_fd = tmpdir.join('Githubfile')
-    config_fd.write(GITHUB_FILE)
+    config_fd.write(dedent(u'''\
+    [core]
+    owner = jacquerie
+    repo = github-file
+    topics = github, configuration, file
+    '''))
+
+    result = runner.invoke(cli, ['update', '-f', str(config_fd)])
+
+    assert 0 == result.exit_code
+
+
+@pytest.mark.vcr()
+def test_update_can_configure_which_features_to_use(runner, tmpdir):
+    config_fd = tmpdir.join('Githubfile')
+    config_fd.write(dedent(u'''\
+    [core]
+    owner = jacquerie
+    repo = github-file
+
+    [features]
+    has_issues = true
+    has_projects = false
+    has_wiki = false
+    '''))
+
+    result = runner.invoke(cli, ['update', '-f', str(config_fd)])
+
+    assert 0 == result.exit_code
+
+
+@pytest.mark.vcr()
+def test_update_can_configure_the_issue_labels(runner, tmpdir):
+    config_fd = tmpdir.join('Githubfile')
+    config_fd.write(dedent(u'''\
+    [core]
+    owner = jacquerie
+    repo = github-file
+
+    [issues]
+    labels = enhancement, #a2eeef
+        bug, #d73a4a
+        question, #d876e3
+    '''))
+
+    result = runner.invoke(cli, ['update', '-f', str(config_fd)])
+
+    assert 0 == result.exit_code
+
+
+@pytest.mark.vcr()
+def test_update_can_configure_the_merge_policy(runner, tmpdir):
+    config_fd = tmpdir.join('Githubfile')
+    config_fd.write(dedent(u'''\
+    [core]
+    owner = jacquerie
+    repo = github-file
+
+    [merges]
+    allow_squash_merge = false
+    allow_merge_commit = false
+    allow_rebase_merge = true
+    '''))
 
     result = runner.invoke(cli, ['update', '-f', str(config_fd)])
 
